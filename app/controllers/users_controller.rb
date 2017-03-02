e0ff57887aa49819764e118e9296a8ab17fddc56
KotMeow <kaamil.kot@gmail.com>
2017-03-02 22:00:20 +0100

    login/logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,13 @@ class UsersController < ApplicationController
     def new
         @user = User.new
     end
-
+    def edit
+        @user = User.find(params[:id])
+    end
     def create
         @user = User.new(user_params)
         if @user.save
+            log_in @user
             flash[:success] = "Welcome! Your account has been registered!"
             redirect_to languages_path 
         else
