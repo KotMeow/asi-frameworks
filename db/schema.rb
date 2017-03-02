9a1f9df3080fe2f8dd22b554f4519a5ce2cb50a8
KotMeow <kaamil.kot@gmail.com>
2017-03-02 20:00:43 +0100

    added user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170302013302) do
+ActiveRecord::Schema.define(version: 20170302184855) do
 
   create_table "frameworks", force: :cascade do |t|
     t.string   "name"
@@ -29,6 +29,13 @@ ActiveRecord::Schema.define(version: 20170302013302) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
